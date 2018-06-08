597d4c7708fe89ded9f978900478ffc0d072bf53
Alfredo Ramirez <34361196+ramirezalfredo@users.noreply.github.com>
2018-06-07 22:50:07 -0600

    changed Dockerfiles to remove propertiesbuilder.sh

--- api/kubernetes/configmap.sh
+++ api/kubernetes/configmap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl create configmap api-properties --from-file=Hygieia/kubernetes/api.properties
+kubectl create configmap api-properties --from-file=Hygieia/kubernetes/application.properties
 #kubectl run nginx --image=nginx
